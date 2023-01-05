b20b161314a393d161c2c86aba71198697435377
Bananek96 <Banasiak_Jakub@wp.pl>
2023-01-05 16:59:48 +0100

    Logout & Register

--- users/views.py
+++ users/views.py
@@ -30,7 +30,7 @@ def loguj_user(request):
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('forum:index')
     template_name = 'users/loguj_user.html'
 
 
